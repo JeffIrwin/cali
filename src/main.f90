aced26d1dccbeea7328ab19488701bbf8770e274
Jeff Irwin <jirwin505@gmail.com>
2023-04-22 11:16:34 -0400

    get font filename from cmd arg

--- src/main.f90
+++ src/main.f90
@@ -1,7 +1,20 @@
 
+!===============================================================================
+
+module cali_m
+
+	integer :: EXIT_SUCCESS = 0, EXIT_FAILURE = -1
+
+end module cali_m
+
+!===============================================================================
+
 program main
 
 	implicit none
+
+	character(len = 1024) :: buffer
+	character(len = :), allocatable :: argv
 
 	integer :: argc
 
@@ -10,6 +23,23 @@ program main
 
 	argc = command_argument_count()
 	print *, 'argc = ', argc
+	if (argc /= 1) then
+		write(*,*) 'Error: bad cmd args'
+		write(*,*) 'Usage:'
+		write(*,*) '	cali path/to/font.ttf'
+		write(*,*)
+		call exit(EXIT_FAILURE)
+	end if
+
+	call get_command_argument(1, buffer, iostat = io)
+	if (io /= EXIT_SUCCESS) then
+	end if
+	argv = trim(buffer)
+	print *, 'argv = ', argv
+
+	call exit(EXIT_SUCCESS)
 
 end program main
+
+!===============================================================================
 
