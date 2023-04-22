5e2c6eeae0631229af525fd75154c3524f44de8f
Jeff Irwin <jirwin505@gmail.com>
2023-04-22 11:35:21 -0400

    read the first two numbers from the ttf header

--- src/main.f90
+++ src/main.f90
@@ -18,7 +18,10 @@ program main
 	character(len = 1024) :: buffer
 	character(len = :), allocatable :: argv
 
-	integer :: argc, io
+	integer :: argc, io, ittf
+
+	integer(kind = 4) :: scalar_type
+	integer(kind = 2) :: num_tables
 
 	write(*,*) 'cali 0.0.1'
 	write(*,*)
@@ -29,16 +32,31 @@ program main
 		write(*,*) 'Error: bad cmd args'
 		write(*,*) 'Usage:'
 		write(*,*) '	cali path/to/font.ttf'
-		write(*,*)
 		call exit(EXIT_FAILURE)
 	end if
 
 	call get_command_argument(1, buffer, status = io)
 	if (io /= EXIT_SUCCESS) then
+		write(*,*) 'Error: get cmd arg'
+		call exit(EXIT_FAILURE)
 	end if
 	argv = trim(buffer)
 	print *, 'argv = ', argv
 
+	open(newunit = ittf, file = argv, action = 'read', iostat = io, access = 'stream')
+	if (io /= EXIT_SUCCESS) then
+		write(*,*) 'Error: cannot open file "', argv, '"'
+		call exit(EXIT_FAILURE)
+	end if
+
+	read(ittf) scalar_type
+	read(ittf) num_tables
+
+	! TODO: interpret as unsigned instead of signed ints
+	print *, 'scalar_type = ', scalar_type
+	print *, 'num_tables  = ', num_tables
+
+	close(ittf)
 	call exit(EXIT_SUCCESS)
 
 end program main
