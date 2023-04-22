b1f3c6f0472302e335f2b9a3db91fbec0837e063
Jeff Irwin <jirwin505@gmail.com>
2023-04-22 13:52:22 -0400

    read tables and print tags

--- src/main.f90
+++ src/main.f90
@@ -5,6 +5,59 @@ module cali_m
 
 	integer :: EXIT_SUCCESS = 0, EXIT_FAILURE = -1
 
+
+contains
+
+!===============================================================================
+
+function read_u32(unit) result(u32)
+
+	! Fortran does not have unsigned ints.  Read a signed int, cast it to a
+	! bigger int size, then re-interpret as the correct positive value
+
+	integer, intent(in) :: unit
+
+	integer(kind = 8) :: u32
+
+	!********
+
+	integer(kind = 4) :: i32
+	integer(kind = 8) :: i64
+
+	u32 = 0
+
+	read(unit) i32
+	i64 = i32
+	!print *, 'i64 = ', i64
+	if (i64 < 0) i64 = i64 + huge(i32)
+	u32 = i64
+
+end function read_u32
+
+!===============================================================================
+
+function read_u16(unit) result(u16)
+
+	integer, intent(in) :: unit
+
+	integer(kind = 8) :: u16
+
+	!********
+
+	integer(kind = 2) :: i16
+	integer(kind = 8) :: i64
+
+	u16 = 0
+
+	read(unit) i16
+	i64 = i16
+	if (i64 < 0) i64 = i64 + huge(i16)
+	u16 = i64
+
+end function read_u16
+
+!===============================================================================
+
 end module cali_m
 
 !===============================================================================
@@ -15,13 +68,14 @@ program main
 
 	implicit none
 
+	character(len = 4) :: tag
 	character(len = 1024) :: buffer
 	character(len = :), allocatable :: argv
 
-	integer :: argc, io, ittf
+	integer :: argc, io, ittf, i
 
-	integer(kind = 4) :: scalar_type
-	integer(kind = 2) :: num_tables
+	integer(kind = 8) :: scalar_type, checksum, offset, length
+	integer(kind = 8) :: num_tables, search_range, entry_select, range_shift
 
 	write(*,*) 'cali 0.0.1'
 	write(*,*)
@@ -43,18 +97,34 @@ program main
 	argv = trim(buffer)
 	print *, 'argv = ', argv
 
-	open(newunit = ittf, file = argv, action = 'read', iostat = io, access = 'stream')
+	open(newunit = ittf, file = argv, action = 'read', iostat = io, &
+		access = 'stream', convert = 'big_endian')
 	if (io /= EXIT_SUCCESS) then
 		write(*,*) 'Error: cannot open file "', argv, '"'
 		call exit(EXIT_FAILURE)
 	end if
 
-	read(ittf) scalar_type
-	read(ittf) num_tables
+	! TODO: save in offset-tables struct
+	scalar_type  = read_u32(ittf)
+	num_tables   = read_u16(ittf)
+	search_range = read_u16(ittf)
+	entry_select = read_u16(ittf)
+	range_shift  = read_u16(ittf)
 
-	! TODO: interpret as unsigned instead of signed ints
 	print *, 'scalar_type = ', scalar_type
 	print *, 'num_tables  = ', num_tables
+
+	do i = 1, num_tables
+		read(ittf) tag
+		print *, 'tag = ', tag
+
+		! TODO: save in array of table structs
+		checksum = read_u32(ittf)
+		offset   = read_u32(ittf)
+		length   = read_u32(ittf)
+
+		!stop
+	end do
 
 	close(ittf)
 	call exit(EXIT_SUCCESS)
