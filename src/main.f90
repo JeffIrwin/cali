28508d3121d69db1d1b5ebad479b74e6a672e5e6
Jeff Irwin <jirwin505@gmail.com>
2023-06-03 11:58:17 -0400

    add fn to get next cmd arg

--- src/main.f90
+++ src/main.f90
@@ -23,17 +23,42 @@ module app_m
 
 	type args_t
 
-		character(len = :), allocatable :: ttf_file
-		character(len = :), allocatable :: language
+		character(len = :), allocatable :: ttf_file, language, out_file
 
 		logical :: &
 			waterfall = .false., &
 			version   = .false., &
+			lout_file = .false., &
 			help      = .false.
 
 	end type args_t
 
 contains
+
+!===============================================================================
+
+subroutine get_next_arg(i, argv0, argv)
+	integer, intent(inout) :: i
+	character(len = :), allocatable, intent(out) :: argv
+	character(len = *), intent(in) :: argv0
+	!********
+	character(len = 1024) :: buffer
+	integer :: io
+
+	!i = 0
+	!argv = ""
+	!io = 0
+
+	i = i + 1
+	call get_command_argument(i, buffer, status = io)
+	if (io /= EXIT_SUCCESS) then
+		write(*,*) ERROR//"cannot get cmd arg after """//argv0//""""
+		call exit(EXIT_FAILURE)
+	end if
+	argv = trim(buffer)
+	!print *, "argv = ", argv
+
+end subroutine get_next_arg
 
 !===============================================================================
 
@@ -43,10 +68,9 @@ function parse_args() result(args)
 
 	!********
 
-	character(len = 1024) :: buffer
-	character(len = :), allocatable :: argv
+	character(len = :), allocatable :: argv, argv0
 
-	integer :: i, io, argc, ipos
+	integer :: i, argc, ipos
 
 	logical :: lerror = .false.
 
@@ -56,37 +80,27 @@ function parse_args() result(args)
 	argc = command_argument_count()
 	!print *, "argc = ", argc
 
-	i = 0
+	i = -1
 	ipos = 0
-	do while (i < argc)
-		i = i + 1
+	call get_next_arg(i, "", argv)
+	argv0 = argv
 
-		call get_command_argument(i, buffer, status = io)
-		if (io /= EXIT_SUCCESS) then
-			write(*,*) ERROR//"cannot get cmd arg"
-			call exit(EXIT_FAILURE)
-		end if
-		argv = trim(buffer)
-		!print *, "argv = ", argv
+	do while (i < argc)
+		call get_next_arg(i, argv0, argv)
+		argv0 = argv
 
 		! TODO: more args:
 		!   - color randomizer seed, relative salt or absolute
 		!   - output filename/dir
 
 		select case (argv)
-		case ("-h", "--help")
+		case ("-h", "--help", "-help")
 			args%help = .true.
 
 		case ("-l", "--language")
-			! TODO: fn.  helpful error if i > argc
-			i = i + 1
-			call get_command_argument(i, buffer, status = io)
-			if (io /= EXIT_SUCCESS) then
-				write(*,*) ERROR//"cannot get cmd arg"
-				call exit(EXIT_FAILURE)
-			end if
-
-			args%language = trim(buffer)
+			call get_next_arg(i, argv0, argv)
+			argv0 = argv
+			args%language = argv
 			! TODO: check language is in langs
 
 		case ("--version")
@@ -101,8 +115,11 @@ function parse_args() result(args)
 
 			if (ipos == 1) then
 				args%ttf_file = argv
+			else if (ipos == 2) then
+				args%lout_file = .true.
+				args%out_file  = argv
 			else
-				write(*,*) ERROR//"unknown positional arg `"//argv//"`"
+				write(*,*) ERROR//"unknown argument `"//argv//"`"
 				lerror = .true.
 			end if
 
@@ -153,10 +170,14 @@ program main
 		to_str(CALI_MAJOR)//"."// &
 		to_str(CALI_MINOR)//"."// &
 		to_str(CALI_PATCH)
-	write(*,*)
 
 	args = parse_args()
-	if (args%version .or. args%help) call exit(EXIT_SUCCESS)
+	if (args%version .or. args%help) then
+		write(*,*) "Homepage: github.com/JeffIrwin/cali"
+		write(*,*)
+		call exit(EXIT_SUCCESS)
+	end if
+	write(*,*)
 
 	if (args%waterfall) then
 		call waterfall(args%ttf_file, 12.d0, 64.d0, 8, args%language)
@@ -164,6 +185,8 @@ program main
 		! TODO: lang arg
 		call specimen(args%ttf_file)
 	end if
+
+	if (args%lout_file) print *, "out_file = ", args%out_file
 
 	write(*,*) FG_BRIGHT_GREEN//"Finished cali"//COLOR_RESET
 	write(*,*)
